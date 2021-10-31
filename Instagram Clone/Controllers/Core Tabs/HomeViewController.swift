74d5c071c3ececb79af577964a6cc663816e631d
wassim <b.wassim1993@hotmail.fr>
2021-10-31 17:14:00 +0100

    Initial Commit

---
+++ Instagram Clone/Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram Clone
+//
+//  Created by wassim on 30/10/2021.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
