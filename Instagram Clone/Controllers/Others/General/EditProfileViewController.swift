7f6d84a30d69f676b8b52a70b914a9481cfa48df
wassim <b.wassim1993@hotmail.fr>
2021-11-04 18:12:39 +0100

    adding edit profile & localized strings

--- Instagram Clone/Controllers/Others/General/EditProfileViewController.swift
+++ Instagram Clone/Controllers/Others/General/EditProfileViewController.swift
@@ -9,13 +9,40 @@ import UIKit
 
 class EditProfileViewController: UIViewController {
 
+    @IBOutlet weak var cancelBtn: UIBarButtonItem!
+    @IBOutlet weak var saveBtn: UIBarButtonItem!
+    @IBOutlet weak var changePicBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        cancelBtn.title = AppHelper.getLocalizeString(str: "cancel_title")
+        saveBtn.title = AppHelper.getLocalizeString(str: "save_title")
+        changePicBtn.setTitle(AppHelper.getLocalizeString(str: "change_pic_btn_title"), for: .normal)
 
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func changePicAction(_ sender: Any) {
+        let actionSheet =  UIAlertController(title: AppHelper.getLocalizeString(str: "change_pic_btn_title"), message: AppHelper.getLocalizeString(str: "action_sheet_msg"), preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: AppHelper.getLocalizeString(str: "action_sheet_action1"), style: .default, handler: { _ in
 
+        }))
+        actionSheet.addAction(UIAlertAction(title: AppHelper.getLocalizeString(str: "action_sheet_action2"), style: .default, handler: {_ in
+
+        }))
+        actionSheet.addAction(UIAlertAction(title: AppHelper.getLocalizeString(str: "action_sheet_action3"), style: .destructive, handler: nil))
+
+        /// this two line for preventing crash on ipad when presenting the sheet
+        actionSheet.popoverPresentationController?.sourceView = view
+        actionSheet.popoverPresentationController?.sourceRect = view.bounds
+
+        present(actionSheet, animated: true)
+    }
+
+    @IBAction func cancelAction(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func saveAction(_ sender: Any) {
+    }
     /*
     // MARK: - Navigation
 
