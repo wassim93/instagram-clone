dfd644e764a644d4089021476f9aef765e1c0c48
wassim <b.wassim1993@hotmail.fr>
2021-11-07 00:13:31 +0100

    adding tableview ui for post controller

--- Instagram Clone/Controllers/Others/General/PostViewController.swift
+++ Instagram Clone/Controllers/Others/General/PostViewController.swift
@@ -9,7 +9,9 @@ import UIKit
 
 class PostViewController: UIViewController {
 
+    //FIXME: missing custom cell for this tableview
 
+    @IBOutlet weak var tableview: UITableView!
     var model:UserPost?
     init(model:UserPost?) {
         self.model =  model
@@ -24,8 +26,32 @@ class PostViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableview.delegate = self
+        tableview.dataSource = self
     }
     
+
+
+}
+
+
+extension PostViewController:UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableview.dequeueReusableCell(withIdentifier: "", for: indexPath)
+
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableview.deselectRow(at: indexPath, animated: true)
+    }
 
 
 }
